f64cef6217a844259c45d08025535858313a0e54
Kilf0 <11zaneh11@gmail.com>
2025-10-09 17:17:17 -0400

    glazing my contact page fr

--- contact.js
+++ contact.js
@@ -13,3 +13,40 @@
 	}
   });
 });
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+	
+const textBtn = document.getElementById("text-button");
+const textPhone = document.getElementById("text-phone");
+const callBtn = document.getElementById("call-button");
+const callPhone = document.getElementById("call-phone");
+
+
+	textBtn.addEventListener("click", () => {
+	
+	textPhone.classList.add("show")
+	textBtn.classList.add("open")
+	sessionStorage.setItem("textClicked", "true");
+	
+});
+		if (sessionStorage.getItem("textClicked")) {
+	textBtn.classList.add("open");
+	textPhone.classList.add("show");
+  }
+
+
+	callBtn.addEventListener("click", () => {
+ 		
+	 callPhone.classList.add("show")
+	 callBtn.classList.add("open")
+	 sessionStorage.setItem("callClicked", "true");
+	 
+ });
+		 if (sessionStorage.getItem("callClicked")) {
+	 callBtn.classList.add("open");
+	 callPhone.classList.add("show");
+   }
+});
